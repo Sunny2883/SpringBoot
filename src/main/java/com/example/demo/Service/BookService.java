bab41129f82b73894f13c42db43d211fd8dc1bee
SUNNY BHUSHAN <sunnybhushan9185@gmail.com>
2024-09-20 17:51:39 +0530

    Change task definition file

---
+++ src/main/java/com/example/demo/Service/BookService.java
@@ -0,0 +1,31 @@
+package com.example.demo.Service;
+
+import java.util.List;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+
+import com.example.demo.Model.Book;
+import com.example.demo.Repository.BookRepository;
+
+@Service
+public class BookService {
+    @Autowired
+    private BookRepository bookRepository;
+
+    public List<Book> getAllBooks() {
+        return bookRepository.findAll();
+    }
+
+    public Book getBookById(Long id) {
+        return bookRepository.findById(id).orElse(null);
+    }
+
+    public Book addBook(Book book) {
+        return bookRepository.save(book);
+    }
+
+    public void deleteBook(Long id) {
+        bookRepository.deleteById(id);
+    }
+}
