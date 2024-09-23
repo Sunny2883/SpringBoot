30593b308da736e8b9d56a3a1566fcd4ed5c298b
SUNNY BHUSHAN <sunnybhushan9185@gmail.com>
2024-09-23 17:37:43 +0530

    Add cors origin

--- src/main/java/com/example/demo/Controller/Bookcontroller.java
+++ src/main/java/com/example/demo/Controller/Bookcontroller.java
@@ -4,6 +4,7 @@ import java.util.List;
 
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.CrossOrigin;
 import org.springframework.web.bind.annotation.DeleteMapping;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
@@ -15,6 +16,7 @@ import org.springframework.web.bind.annotation.RestController;
 import com.example.demo.Model.Book;
 import com.example.demo.Service.BookService;
 
+@CrossOrigin(origins = "http://localhost:3000") // Allow requests from this origin
 @RestController
 @RequestMapping("/api/books")
 public class Bookcontroller {
