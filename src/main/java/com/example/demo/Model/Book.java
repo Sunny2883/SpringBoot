5de2dba68414da05b4d354ebbbc17bd93a6a46bb
SUNNY BHUSHAN <sunnybhushan9185@gmail.com>
2024-09-24 11:22:12 +0530

    Add new fields

--- src/main/java/com/example/demo/Model/Book.java
+++ src/main/java/com/example/demo/Model/Book.java
@@ -1,5 +1,7 @@
 package com.example.demo.Model;
 
+import java.time.LocalDate;
+
 import jakarta.persistence.Entity;
 import jakarta.persistence.GeneratedValue;
 import jakarta.persistence.GenerationType;
@@ -13,6 +15,16 @@ public class Book {
     private String title;
     private String author;
     private String isbn;
+    private String description;
+    private LocalDate publicationDate;
+    private String publisher;
+    private Double price;
+    private int pageCount;
+    private String language;
+    private Double averageRating;
+    private boolean inStock;
+    private String imageUrl;
+
 
     // Getters and setters
     public Long getId() {
@@ -45,5 +57,77 @@ public class Book {
 
     public void setIsbn(String isbn) {
         this.isbn = isbn;
+    }
+
+    public String getDescription(){
+        return description;
+    }
+
+    public void setDescription(String description){
+        this.description= description;
+    }
+
+    public double getPrice(){
+        return price;
+    }
+
+    public void setPrice(double price){
+        this.price= price;
+    }
+
+    public String getPublisher(){
+        return publisher;
+
+    }
+
+    public void setPublisher(String publisher){
+        this.publisher= publisher;
+    }
+
+    public int getPageCount() {
+        return pageCount;
+    }
+    
+    public void setPageCount(int pageCount) {
+        this.pageCount = pageCount;
+    }
+    
+    public String getLanguage() {
+        return language;
+    }
+    
+    public void setLanguage(String language) {
+        this.language = language;
+    }
+    
+    public Double getAverageRating() {
+        return averageRating;
+    }
+    
+    public void setAverageRating(Double averageRating) {
+        this.averageRating = averageRating;
+    }
+    
+    public boolean isInStock() {
+        return inStock;
+    }
+    
+    public void setInStock(boolean inStock) {
+        this.inStock = inStock;
+    }
+    
+    public LocalDate getPublicationDate() {
+        return publicationDate;
+    }
+    
+    public void setPublicationDate(LocalDate publicationDate) {
+        this.publicationDate = publicationDate;
+    }
+    public String getImageUrl(){
+        return imageUrl;
+    }
+
+    public void setImageUrl(String imageUrl){
+        this.imageUrl= imageUrl;
     }
 }
